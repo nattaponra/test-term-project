1980914d7dfef323d452a2753b94376c21b89293
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-11-22 15:29:33 +0700

    Remove helper

--- Main-Implemented/test/Step3/Step3Test.php
+++ Main-Implemented/test/Step3/Step3Test.php
@@ -10,8 +10,13 @@ final class Step3Test extends TestCase
 
     function testWithdrawBalanceHasInsufficient()
     {
-       $TestHelper = new TestHelper();
-       //$TestHelper->createUser("9999911111","2561","TEST TEST",1000);
+        $TestHelper = new TestHelper();
+        $TestHelper->createUser("9999911111","2561","TEST TEST",1000);
+        $TestHelper->removeUser("9999911111","2561");
+
+        $withdrawal = new Withdrawal();
+
+
         $this->assertTrue(true);
     }
 
--- Main-Implemented/test/TestHelper.php
+++ Main-Implemented/test/TestHelper.php
@@ -6,13 +6,18 @@ class TestHelper extends DBConnection
     function createUser($accNum, $pin, $name, $balance = 0, $waterCharge = 0, $electricCharge = 0, $phoneCharge = 0)
     {
         $con = DBConnection::getInstance();
-
-        $stmt = $con->prepare("TRUNCATE ACCOUNT");
-        $stmt->execute();
-
         $stmt = $con->prepare(
             "INSERT INTO `ACCOUNT` (`no`, `pin`, `name`, `balance`, `waterCharge`, `electricCharge`, `phoneCharge`) VALUES
             ('$accNum', '$pin', '$name', $balance, $waterCharge, $electricCharge, $phoneCharge)"
+        );
+        $stmt->execute();
+    }
+
+    function removeUser($accNum, $pin)
+    {
+        $con = DBConnection::getInstance();
+        $stmt = $con->prepare(
+            "DELETE FROM `ACCOUNT` WHERE no='$accNum' AND pin='$pin'"
         );
         $stmt->execute();
     }
